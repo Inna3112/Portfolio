428a02f60d22275000109ec6646eb2d47c4a5d7e
Inna3112 <innula3112@gmail.com>
2021-08-09 15:40:55 +0300

    Add sass

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Projects.module.css';
+import s from './Projects.module.scss';
 import sContainer from './../../common/stiles/Container.module.css'
 import {Title} from '../../common/components/title/Title';
 import {ProjectType} from "../../data/data";
