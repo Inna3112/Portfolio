83a3461ad96b9ef08a96a1efe533d4b6b70c5acb
Inna3112 <innula3112@gmail.com>
2021-08-12 15:08:16 +0300

    Updated

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,8 +8,8 @@ export const Main = () => {
         <div className={s.mainBlock}>
             <div className={sContainer.container}>
                 <div className={s.greeting}>
-                    <h6>Hi there!</h6>
-                    <h1>I'm Inna Fomichova</h1>
+                    <h6>Hi there, I'm</h6>
+                    <h1>Inna Fomichova</h1>
                     <h2>Frontend developer.</h2>
                 </div>
                 <div className={s.photo}></div>
