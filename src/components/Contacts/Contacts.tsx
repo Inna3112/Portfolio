16e996385c40a865482b526ab6550e3447e601c2
Inna3112 <innula3112@gmail.com>
2021-10-11 11:55:55 +0300

    added submitHandler

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import s from './Contacts.module.scss';
 import sContainer from '../../common/stiles/Container.module.scss'
 import {Title} from '../../common/components/title/Title';
 import {FormInfoItem} from '../../data/data';
 import {FormContact} from './Contact/FormContact';
+import axios from 'axios';
 
 
 type PropsType = {
@@ -11,15 +12,33 @@ type PropsType = {
 }
 
 export const Contacts: React.FC<PropsType> = ({formInfoItems}) => {
+    const [nameValue, setNameValue] = useState('')
+    const [emailValue, setEmailValue] = useState('')
+    const [commentValue, setCommentValue] = useState('')
+
+    const onSubmitHandler = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        axios.post('http://localhost:3010/sendMessage', {nameValue, emailValue, commentValue})
+            .then(()=> {
+                alert('Message has been sent')
+            })
+    }
+
     return (
         <div id='contacts' className={s.contactsBlock}>
             <div className={`${sContainer.container} ${s.contactsContainer}`}>
                 <Title text={'Contacts'}/>
                 <div className={s.contacts}>
-                    <form className={s.contactForm}>
-                        <input className={s.formControl} type='text' placeholder={'Name'}/>
-                        <input className={s.formControl} type='text' placeholder={'Email'}/>
-                        <textarea className={s.formControl} placeholder={'Comment'}/>
+                    <form className={s.contactForm} onSubmit={onSubmitHandler}>
+                        <input className={s.formControl} type='text' placeholder={'Name'}
+                               value={nameValue}
+                               onChange={(e) => setNameValue(e.currentTarget.value)}/>
+                        <input className={s.formControl} type='text' placeholder={'Email'}
+                               value={emailValue}
+                               onChange={(e) => setEmailValue(e.currentTarget.value)}/>
+                        <textarea className={s.formControl} placeholder={'Comment'}
+                                  value={commentValue}
+                                  onChange={(e) => setCommentValue(e.currentTarget.value)}/>
                         <button type='submit'>Send your message</button>
                     </form>
                     <div className={s.contactsInfo}>
